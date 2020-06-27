b49912df69857a2ee30b34f87d06792989aa208f
Gagan <gagande90@gmail.com>
2020-06-27 12:42:55 +1000

    assets

---
+++ blogs/python/1.py
@@ -0,0 +1,31 @@
+# Concatenaton of tuples 
+Tuple1 = (0, 1, 2, 3) 
+Tuple2 = ('Geek', 'of', 'Python') 
+
+Tuple3 = Tuple1 + Tuple2 
+
+# Printing first Tuple 
+print("Tuple 1: ") 
+print(Tuple1) 
+
+# Printing Second Tuple 
+print("\nTuple2: ") 
+print(Tuple2) 
+
+# Printing Final Tuple 
+print("\nTuples after Concatenaton: ") 
+print(Tuple3) 
+
+#Output of Program
+Tuple 1: 
+(0, 1, 2, 3)
+
+Tuple2: 
+('Geek', 'of', 'Python')
+
+Tuples after Concatenaton: 
+(0, 1, 2, 3, 'Geek', 'of', 'Python')
+
+
+
+
